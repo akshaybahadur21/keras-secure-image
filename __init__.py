850b684ba944d4a5e02e86588d1a5b064cb1181d
Akshay Bahadur <akshay.dominator@gmail.com>
2019-05-05 16:14:53 +0530

    structure change

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from secure_image import encrypt_directory, decrypt_img, transform, transform_img, perform_rotation, rot
---
+++ keras_secure_image/__init__.py
@@ -0,0 +1,2 @@
+from keras_secure_image.secure_image import encrypt_directory, decrypt_img, transform_img, transform, rot, \
+    perform_rotation
